403e8ff0290f271bca57fbd6994e53278e427c01
Balu Varanasi <vabasu@gmail.com>
2013-04-28 00:17:29 +0530

    removed BaseChart from flot.py and added as_html method and commited with other minor fixes

--- graphos/renderers/flot.py
+++ graphos/renderers/flot.py
@@ -2,53 +2,19 @@ import json
 import random
 import string
 
+from .base import BaseChart
 
-class BaseChart(object):
-
-    def __init__(self, data_source, html_id=None, options={}, *args, **kwargs):
-        self.data_source = data_source
-        self.data = data_source.get_data()
-        self.header = data_source.get_header()
-        self.options = options
-        random_letter = lambda: random.choice(string.ascii_letters)
-        self.html_id = html_id or "".join([random_letter()
-                                          for el in range(10)])
-
-    def get_html_id(self):
-        return self.html_id
-
-    def get_serieses(self):
-        pass
-
-    def get_options(self):
-        pass
-
-    def get_template(self):
-        pass
-
-    def get_serieses_json(self):
-        return json.dumps(self.get_serieses())
-
-    def get_options_json(self):
-        return json.dumps(self.options)
+from django.template.loader import render_to_string
 
 
 class LineChart(BaseChart):
     """ LineChart """
 
-    # data = [
-    #             ['Year', 'Sales', 'Expenses'],
-    #             ['2004',  1000,      400],
-    #             ['2005',  1170,      460],
-    #             ['2006',  660,       1120],
-    #             ['2007',  1030,      540]
-    #         ]
-
     def __init__(self, *args, **kwargs):
         super(LineChart, self).__init__(*args, **kwargs)
 
     def get_serieses(self):
-        data_only = self.data[1:]
+        data_only = self.get_data()[1:]
         first_column = [el[0] for el in data_only]
         serieses = []
         for i in range(1, len(self.header)):
@@ -70,9 +36,12 @@ class LineChart(BaseChart):
     def get_series_objects_json(self):
         return json.dumps(self.get_series_objects())
 
-    def get_options(self):
-        pass
-
     def get_template(self):
         template = 'charts/line_chart.html'
         return template
+
+    def as_html(self):
+        series = self.get_series_objects_json()
+        options = self.get_options_json()
+        context = { 'series': series, 'html_id': self.html_id, 'options': options}
+        return render_to_string(self.get_template(), context)
