21839a7662b6fa30719204c995309c35097a4272
Shabda Raaj <shabda@agiliq.com>
2013-04-27 17:51:15 +0530

    Graphos doesn't need a url

--- graphos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-from django.conf import settings
-admin.autodiscover()
-
-urlpatterns = patterns('',
-)
