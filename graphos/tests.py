ffb0c4e4a093cbd2bca4f56aea37995a4cf132fc
Shabda Raaj <shabda@agiliq.com>
2013-04-27 13:24:34 +0530

    Create the skeleton of the BaseDataSource

--- graphos/tests.py
+++ graphos/tests.py
@@ -3,10 +3,15 @@ from graphos.templatetags.graphos_tags import plot_model_series
 from graphos.templatetags.graphos_tags import plot_redis_series
 
 from .sources.base import BaseDataSource
+from .exceptions import GraphosException
 
 
 class TestSources(TestCase):
     def test_base_data_source(self):
         data_source = BaseDataSource()
         self.assertTrue(hasattr(data_source, "get_data"))
-        self.assertRaises(Exception, data_source.get_data,)
+        self.assertRaises(GraphosException, data_source.get_data,)
+        self.assertTrue(hasattr(data_source, "get_header"))
+        self.assertRaises(GraphosException, data_source.get_header)
+        self.assertTrue(hasattr(data_source, "get_first_column"))
+        self.assertRaises(GraphosException, data_source.get_first_column)
