1d48cbede47faaecde030fb797d88fbc57e3532c
Balu Varanasi <vabasu@gmail.com>
2013-04-27 13:14:52 +0530

    moved contents of core.models to graphos_demo.models

--- graphos_demo/models.py
+++ graphos_demo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeSeries(models.Model):
+    """
+    value is used as y axis for the graph
+    """
+    value = models.IntegerField()
