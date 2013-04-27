04941f06b45b3caa2b3f4e9d3108c4c96c9d9fa1
Shabda Raaj <shabda@agiliq.com>
2013-04-27 20:21:38 +0530

    Simplified demo view.

--- demo/views.py
+++ demo/views.py
@@ -1,36 +1,25 @@
-from django.core.urlresolvers import reverse
-from django.shortcuts import render_to_response
-from django.template.context import RequestContext
+from django.shortcuts import render
+
 from graphos.renderers.flot import LineChart
 from graphos.renderers import gchart
 from graphos.sources.simple import SimpleDataSource
 
+data = [
+       ['Year', 'Sales', 'Expenses'],
+       [2004, 1000, 400],
+       [2005, 1170, 460],
+       [2006, 660, 1120],
+       [2007, 1030, 540], ]
+
 
 def home(request):
-    data =  [
-            ['Year', 'Sales', 'Expenses'],
-            [2004, 1000, 400],
-            [2005, 1170, 460],
-            [2006, 660, 1120],
-            [2007, 1030, 540]
-            ]
-
     chart = LineChart(SimpleDataSource(data=data), html_id="line_chart")
     g_chart = gchart.LineChart(SimpleDataSource(data=data))
-    c = RequestContext(request)
-    return render_to_response('home.html', {'chart': chart,
-                                            'g_chart': g_chart},
-                              context_instance=c)
+    return render(request, 'home.html', {'chart': chart,
+                                         'g_chart': g_chart},
+                  )
+
 
 def tutorial(request):
-    data =  [
-            ['Year', 'Sales', 'Expenses'],
-            [2004, 1000, 400],
-            [2005, 1170, 460],
-            [2006, 660, 1120],
-            [2007, 1030, 540]
-        ]
     chart = LineChart(SimpleDataSource(data=data), html_id="line_chart")
-    c = RequestContext(request,)
-    return render_to_response('tutorial.html', {'chart': chart}, context_instance=c)
-
+    return render(request, 'tutorial.html', {'chart': chart}, )
