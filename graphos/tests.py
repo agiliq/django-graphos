39cadc1988ffd686226cb624ef7d882b1f61f837
Shabda Raaj <shabda@agiliq.com>
2013-04-27 12:59:54 +0530

    Handlers -> Source

--- graphos/tests.py
+++ graphos/tests.py
@@ -1,10 +1,3 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from graphos.templatetags.graphos_tags import plot_model_series
 from graphos.templatetags.graphos_tags import plot_redis_series
