113fa8f3a3d3b6c9d12afcbca4bffe07aed9a8c3
Balu Varanasi <vabasu@gmail.com>
2013-04-27 13:19:38 +0530

    deleted core app

--- core/admin.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 *-*
-from django.contrib import admin
-from core.models import TimeSeries
-admin.site.register(TimeSeries)
--- core/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class TimeSeries(models.Model):
-    """
-    value is used as y axis for the graph
-    """
-    value = models.IntegerField()
