b9a04f08ea85af96fa4c898f45b0ac362c299139
krvc <krvc.it@gmail.com>
2013-04-29 20:20:32 +0530

    Heroku Deployment changes 4

--- settings.py
+++ settings.py
@@ -154,8 +154,8 @@ except ImportError:
     pass
 
 # Parse database configuration from $DATABASE_URL
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
