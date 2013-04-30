08c75bf198ae1b0b181290734dfb5efd0e76b2be
Balu Varanasi <vabasu@gmail.com>
2013-04-30 18:58:30 +0530

    added 'get_mongo_cursor' definition in graphos.utils

--- graphos/utils.py
+++ graphos/utils.py
@@ -1,9 +1,14 @@
 import random
 import string
 
+import pymongo
+
 
 DEFAULT_HEIGHT = 400
 DEFAULT_WIDTH = 800
+
+DB_HOST = ["localhost"]
+DB_PORT = 27017
 
 
 def get_random_string():
@@ -19,3 +24,12 @@ def get_default_options(graph_type="lines"):
                "legend": {"position": 'ne'},
                "title": "Chart"}
     return options
+
+
+def get_mongo_cursor(db_name, collection_name, max_docs=100):
+    db = pymongo.Connection(DB_HOST, DB_PORT)['graphos_mongo']
+    collection = db['zips']
+    cursor = collection.find()
+    if cursor.count >= max_docs:
+        cursor = cursor[0:max_docs]
+    return cursor
