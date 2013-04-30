e49e392f4de8b614eeb8faa60354a59c51699b6e
Balu Varanasi <vabasu@gmail.com>
2013-04-30 18:04:06 +0530

    added some more utilities

--- graphos/utils.py
+++ graphos/utils.py
@@ -1,3 +1,16 @@
+import random
+import string
+
+
+DEFAULT_HEIGHT = 400
+DEFAULT_WIDTH = 800
+
+
+def get_random_string():
+    random_letter = lambda: random.choice(string.ascii_letters)
+    random_string = "".join([random_letter()
+                             for el in range(10)])
+    return random_string
 
 
 def get_default_options(graph_type="lines"):
