561f0d4aadd45723a7647785388923bb12109b61
Balu Varanasi <vabasu@gmail.com>
2013-05-03 23:21:47 +0530

    added time_series_example url

--- demo/urls.py
+++ demo/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     url(r'^gchart/$', 'demo.views.gchart_demo', name='demo_gchart_demo'),
     url(r'^yui/$', 'demo.views.yui_demo', name='demo_yui_demo'),
     url(r'^flot/$', 'demo.views.flot_demo', name='demo_flot_demo'),
-    url(r'^mongodb_source/$', 'demo.views.mongodb_source_demo',
-                              name='demo_mongodb_source_demo'),
+    url(r'^time_series/$', 'demo.views.time_series_demo',
+                           name='demo_time_series_example'),
 )
