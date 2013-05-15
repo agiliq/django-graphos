11ca5ed09e108d912c6b9ecef005a62544d280f7
balu <balu@agiliq.com>
2013-05-16 00:32:12 +0530

    added urls

--- demo_project/demo_project/urls.py
+++ demo_project/demo_project/urls.py
@@ -1,17 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'demo_project.views.home', name='home'),
-    # url(r'^demo_project/', include('demo_project.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include("demo.urls")),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT}),
 )
+
