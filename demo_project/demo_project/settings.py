82ec807e494fbfe5e21c546b1062289cc7392d95
balu <balu@agiliq.com>
2013-05-16 00:31:16 +0530

    fix static url

--- demo_project/demo_project/settings.py
+++ demo_project/demo_project/settings.py
@@ -66,7 +66,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
