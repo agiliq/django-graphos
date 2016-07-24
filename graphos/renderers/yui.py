a0e9b315b29946ebcca283b314f3c7fae135d097
Akshar Raaj <akshar@agiliq.com>
2016-07-24 13:44:25 +0530

    Refactored code

--- graphos/renderers/yui.py
+++ graphos/renderers/yui.py
@@ -22,17 +22,53 @@ class LineChart(BaseYuiChart):
     def get_js_template(self):
         return "graphos/yui/line_chart.html"
 
+    def get_chart_type(self):
+        return "line"
+
+
+class SplineChart(BaseYuiChart):
+    def get_js_template(self):
+        return "graphos/yui/spline_chart.html"
+
+    def get_chart_type(self):
+        return "spline"
+
 
 class BarChart(BaseYuiChart):
     def get_js_template(self):
         return "graphos/yui/bar_chart.html"
+
+    def get_chart_type(self):
+        return "bar"
 
 
 class ColumnChart(BaseYuiChart):
     def get_js_template(self):
         return "graphos/yui/column_chart.html"
 
+    def get_chart_type(self):
+        return "column"
+
 
 class PieChart(BaseYuiChart):
     def get_js_template(self):
         return "graphos/yui/pie_chart.html"
+
+    def get_chart_type(self):
+        return "pie"
+
+
+class AreaChart(BaseYuiChart):
+    def get_js_template(self):
+        return "graphos/yui/area_chart.html"
+
+    def get_chart_type(self):
+        return "area"
+
+
+class AreaSplineChart(BaseYuiChart):
+    def get_js_template(self):
+        return "graphos/yui/area_spline_chart.html"
+
+    def get_chart_type(self):
+        return "areaspline"
