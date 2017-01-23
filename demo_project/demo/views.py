fa7978bc383821537f4dc53fe6455626c851e83d
Akshar Raaj <akshar@agiliq.com>
2017-01-23 15:36:18 +0530

    Added comments

--- demo_project/demo/views.py
+++ demo_project/demo/views.py
@@ -307,7 +307,9 @@ class HighChartsDemo(Demo):
             'scatter_chart': self.renderer.ScatterChart(simple_data_source),
             'log_chart': self.renderer.LogarithmicChart(data_source),
             'multi_axis_chart': self.renderer.MultiAxisChart(SimpleDataSource(secondary_data)),
+            # If you want highmap_chart to be bubble chart, then add 'map_type': 'mapbubble' to options.
             'highmap_chart': self.renderer.HighMap(SimpleDataSource(map_data_us), options={'colorAxis': {'minColor': '#efecf3', 'maxColor': '#990041'}, 'plotOptions': {'map': {'dataLabels': {'enabled': True, 'format': '{point.name}'}}}, 'map_area': 'countries/us/custom/us-all-territories'}),
+            # If you want highmap_chart to be bubble chart, then add 'map_type': 'mapbubble' and 'zKey': 'Seats' to options. Also remove 'allAreas': False.
             'highmap_chart_multi': self.renderer.HighMap(SimpleDataSource(map_data_us_multi_series), options={'colors': ['red'], 'plotOptions': {'map': {'dataLabels': {'enabled': True, 'format': '{point.name}'}, 'allAreas': False, 'tooltip': {'useHTML': True, 'pointFormat': '{point.name} Number of seats: {point.Seats}'}}}, 'map_area': 'countries/us/custom/us-all-territories'}),
         })
         return context
